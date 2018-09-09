de78d3f122fce761849e76336faddff725cddb4f
HelloNYC2016 <zc950@nyu.edu>
2018-09-09 11:57:19 -0700

    Add some components and a routes

---
+++ oj-client/src/app/mock-problem.ts
@@ -0,0 +1,22 @@
+import {Problem} from './models/problem.model';
+
+export const PROBLEMS: Problem[] = [
+  {
+    id: 1,
+    name: 'Two Sum',
+    description: 'Find close two sum',
+    difficulty: 'Medium',
+  },
+  {
+    id: 2,
+    name: 'Three Sum',
+    description: 'Find close three sum',
+    difficulty: 'Medium',
+  },
+  {
+    id: 3,
+    name: 'Longest continuous sequence',
+    description: 'Find Longest Continuous Sequence',
+    difficulty: 'Hard',
+  }
+];
